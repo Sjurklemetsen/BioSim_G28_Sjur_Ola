5bff97696e9faa76ef3745670a9aaa4aa96e5d77
Ola Flesche <olhellen@nmbu.no>
2020-01-09 10:29:43 +0100

    Making tests
    Tests for geography class and its subclasses

--- tests/test_geography.py
+++ tests/test_geography.py
@@ -3,32 +3,42 @@
 __author__ = 'Sjur Spjeld Klemetsen, Ola Flesche Hellenes'
 __email__ = 'sjkl@nmbu.no, olhellen@nmbu.no'
 
+from biosim.Geography import *
+
 
 class GeographyTest:
     """
     Tests for the geography class where we work with the different
     landscapes.
     """
-
     def test_ocean(self):
         """
         Tests if ocean is an instance
         :return:
         """
-        pass
+        o = Ocean()
+        assert issubclass(Ocean, Geography)
+        assert isinstance(o, Ocean)
 
     def test_mountain(self):
         """
         Test if mountain is an instance
         :return:
         """
-        pass
+        m = Mountain()
+        assert issubclass(Mountain, Geography)
+        assert isinstance(m, Mountain)
 
     def test_desert(self):
         """
         Test if the fodder equals zero
         :return:
         """
+        d = Desert()
+        assert issubclass(Desert, Geography)
+        assert isinstance(d, Desert)
+        assert "default param" f_max == 0
+
 
     def test_savannah(self):
         """
@@ -36,7 +46,11 @@ class GeographyTest:
         Test if fodders in the jungle is updated for each cycle
         :return:
         """
-        pass
+        s = Savannah()
+        assert issubclass(Savannah, Geography)
+        assert isinstance(s, Savannah)
+        assert "default param" f_max == 200
+        assert "test if fodder grows to formula each year"
 
     def test_jungle(self):
         """
@@ -45,4 +59,9 @@ class GeographyTest:
         cycle
         :return:
         """
-        pass
+        j = Jungle()
+        j_fodder = Jungle().fodder_replenish()
+        assert issubclass(Jungle, Geography)
+        assert isinstance(j, Jungle)
+        assert "default param" f_max == 800
+        assert j_fodder == "defaut param" f_max
