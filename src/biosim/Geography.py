824ab51d5028aa04b15a523c7b4babf387ae54c5
Sjur klemetsen <sjkl@nmbu.no>
2020-01-11 17:46:23 +0100

    Configurated methods
    added many methods for adding animals, setting parameters and removing animals

--- src/biosim/Geography.py
+++ src/biosim/Geography.py
@@ -14,12 +14,166 @@ class Geography:
     cell eat, migrate and mate. There are also methods for how fodder grows in
     a cell.
     """
+    geo_p = {'f_max': None, 'alpha': None}
+
+    def set_parameter(self, new_parameters):
+        """
+        This method let you set new parameters instead of the default ones
+        :param new_parameters: dictionary with new parameters
+        """
+        for key in new_parameters:
+            self.geo_p[key] = new_parameters
 
     def __init__(self):
-        self.herbivores = []
-        self.carnivores = []
+        self.pop_herbivores = []
+        self.pop_carnivores = []
         self.fodder = 0
 
+    def add_animal(self, animal):
+        """
+        Add an instance of the animal class to the list of herbivores or
+        carnivores.
+        :param animal: An instance of the Fauna subclasses
+        """
+        if type(animal).__name__ == 'Herbivore':
+            self.pop_herbivores.append(animal)
+        elif type(animal).__name__ == 'Carnivore':
+            self.pop_carnivores.append(animal)
+
+    def remove_animals(self):
+        """
+        This method removes the dead animals from a cell
+        """
+        for animal in self.pop_herbivores:
+            if animal.check_death():
+                self.pop_herbivores.pop(animal)
+        for animal in self.pop_carnivores:
+            if animal.check_death():
+                self.pop_carnivores.pop(animal)
+
+    def herbivore_pop(self):
+        """
+        :return: How many herbivores in a cell
+        """
+        return len(self.pop_herbivores)
+
+    def carnivore_pop(self):
+        """
+        :return: How many carnivores in a cell
+        """
+        return len(self.pop_carnivores)
+
+    def total_pop(self):
+        """
+        :return: Total population in a cell
+        """
+        return len(self.pop_herbivores) + len(self.pop_carnivores)
+
+    def remove_fodder(self):
+        """
+        A method that removes the fodder that gets eaten by the animals
+        :return: appetite - How much fodder the animal eat
+        """
+        appetite = Herbivore.p['F']
+
+        if appetite <= self.fodder:
+            self.fodder -= appetite
+            return appetite
+        elif 0 < self.fodder < appetite:
+            self.fodder = 0
+            appetite = self.fodder
+            return appetite
+        else:
+            return 0
+
+    def herbivore_eat(self):
+        """
+        All the herbivores in the cell eat fodder
+        :return:
+        """
+        for animal in self.pop_herbivores:
+            animal.eat()
+            self.remove_fodder()
+
+    def sort_animal_fitness(self, population):
+        """
+        Sort the herbivores in the cell after their fitness
+        :return:
+        """
+        pass
+
+    def carnivore_eat(self):
+        """
+        Carnivore eat
+        :return: 
+        """
+        self.sort_animal_finess(self.pop_carnivores)
+        self.sorted_herbivores()
+        for animal in self.pop_carnivores:
+            elf.sorted_herbivores()
+            animal.eat(weight killed animal) # Carnivore eat and gain weight
+            self.sorted_herbivores()
+            self.herbivore_pop().pop[-1] # Herbivore with worst fitness die
+        pass
+
+class Jungle(Geography):
+    """
+    A jungle cell where carnivore can hunt herbivore and herbivore can eat food
+    Fodder replenish each year to f_max.
+    """
+    geo_p = {'f_max': 800}
+
+    def __init__(self):
+        super().__init__()
+        self.fodder = self.geo_p['f_max']
+
+    def fodder_growth(self):
+        """
+        Replenishes fodder each year
+        :return:
+        """
+        self.fodder = self.f_max
+        pass
+
+    def fodder_eaten(self, fodder_eaten):
+        """
+        A method that removes the fodder that gets eaten by the animals
+        :return:
+        """
+        self.fodder -= fodder_eaten
+    pass
+
+
+
+class Savannah(Geography):
+    """
+    A savannah cell that holds fodder, but can suffer overgrazing
+    """
+    geo_p = {'f_max': 300, 'alpha': 0.3}
+
+    def __init__(self):
+        super().__init__(self)
+
+    def get_fodder(self):
+        pass
+
+    def fodder_growth(self):
+        """
+        Fodder growth for savannah
+        :return:
+        """
+        pass
+
+
+class Desert(Geography):
+    """
+    Area type that holds no fodder, but animals can inhabit the cells
+    """
+
+    def __init__(self):
+        super().__init__(self)
+        pass
+    pass
 
 
 class Ocean(Geography):
@@ -46,92 +200,9 @@ class Mountain:
     pass
 
 
-class Desert(Geography):
-    """
-    Area type that holds no fodder, but animals can inhabit the cells
-    """
-
-    def __init__(self):
-        super().__init__(self)
-        pass
-    pass
-
-
-class Savannah(Geography):
-    """
-    A savannah cell that holds fodder, but can suffer overgrazing
-    """
-
-    def __init__(self):
-        super().__init__(self)
-        self.f_max = 300
-        self.alpha = 0.3
-
-    def get_fodder(self):
-
-
-    def fodder_growth(self):
-        """
-        Fodder growth for savannah
-        :return:
-        """
-        pass
-
-
-class Jungle(Geography):
-    """
-    A jungle cell where carnivore can hunt herbivore and herbivore can eat food
-    Fodder replenish each year to f_max.
-    """
-
-    def __init__(self):
-        self.f_max = 800
-        self.fodder = self.f_max
-
-    def add_animal(self):
-        self.herbivores.append(Herbivore())
-        self.carnivores.append(Carnivore())
-
-    def fodder_growth(self):
-        """
-        Replenishes fodder each year
-        :return:
-        """
-        self.fodder = self.f_max
-
-    def fodder_eaten(self, fodder_eaten):
-        """
-        A method that removes the fodder that gets eaten by the animals
-        :return:
-        """
-        self.fodder -= fodder_eaten
-
-    def fodder_amount(self):
-        return self.fodder
-
-    def herbivore_pop(self):
-        """
-        Check how many animals that currently are residing in this jungle cell
-        """
-        return len(self.herbivores)
-
-    def carnivore_pop(self):
-        return len(self.carnivores)
-
-    def remove_animals(self):
-        """
-        This method removes a the dead animals from a cell
-        :return:
-        """
-        for animal in self.herbivores:
-            if animal.check_death():
-                self.herbivores.pop(animal)
-        for animal in self.carnivores:
-            if animal.check_death():
-                self.carnivores.pop(animal)
 
 
 if __name__ == "__main__":
     geo = Geography()
-    geo.add_animal(population=[Herbivore(), Herbivore()])
-    print(geo.add_animal(population=[]))
+    g = geo.add_animal(Herbivore())
+    print(geo.herbivores)
