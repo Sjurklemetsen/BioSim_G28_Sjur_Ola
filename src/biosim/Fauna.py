3d5e4474c5416c753eb98efc31ad22f934d17c5c
Ola Flesche <olhellen@nmbu.no>
2020-01-13 18:12:23 +0100

    Carnivore eat and tests for it
    struggling with eat function for carnivore

--- src/biosim/Fauna.py
+++ src/biosim/Fauna.py
@@ -186,7 +186,7 @@ class Carnivore(Fauna):
         else:
             return 1
 
-    def eat(self, herb_w):
+    def eat(self, pop_herb):
         """
         The weight of the animal increase every time the animal eat
         The amount of herbivores decrease if a carnivore eats
@@ -194,24 +194,32 @@ class Carnivore(Fauna):
         update fitness
         :return:
         """
-        self.weight += herb_w * self.p['beta']
-        self.update_fitness()
+        new_pop = []
+        w_herb_killed = 0
+
+        for herb in pop_herb[::-1]:
+            if w_herb_killed < self.p['F'] \
+                    and rd.random() < self.prob_eating(herb):
+                self.weight += herb.weight * self.p['beta']
+                self.update_fitness()
+                w_herb_killed += herb.weight
+                pop_herb.remove(herb)
+            else:
+                new_pop.append(herb)
+        # carni spiser ikke mer enn F!
+        return new_pop
 
 
 if __name__ == "__main__":
-    herb = Herbivore(weight=10, age=4)
-    print(herb.fitness)
-    #print(herb.check_migration())
-    carn = Carnivore(weight=60, age=10)
 
-    print(carn.fitness)
+    c = Carnivore(age=10, weight=70)
+    pop_herb = []
+    for n in range(10):
+        pop_herb.append(Herbivore())
+    print(pop_herb)
+    print(c.eat(pop_herb))
+    print(len(pop_herb))
 
-    if carn.fitness <= herb.fitness:
-        print(0)
-    elif 0 < carn.fitness - herb.fitness < carn.p['DeltaPhiMax']:
-        print((carn.fitness - herb.fitness)/carn.p['DeltaPhiMax'])
-    else:
-        print(1)
 
 
     """n_animals = 60
