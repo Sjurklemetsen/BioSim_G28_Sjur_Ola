d405de21c67f6c1a6265bde1a0e9c514826f189e
Sjur klemetsen <sjkl@nmbu.no>
2020-01-14 19:14:53 +0100

    Made a dictionary with coordinates and instances as a map

--- src/biosim/Map.py
+++ src/biosim/Map.py
@@ -12,39 +12,46 @@ class Map:
     Makes a dictionary with coordinate tuples as key and instance classes
     as values
     """
-    def __init__(self, area_type):
-        self.map_dict = self.create_map(area_type)
+
+    def __init__(self):
+        self.map_dict = {}
 
     def create_map(self, area_type):
-        """
-        A method that makes the map containing many cells that contain a
-        geography instance with animals
-        - Check if all edges are area type ocean.
-        - Raise ValueError if you type something else then allowed letters
-        :param: List
-        :return: Dict
-        """
         string = area_type.replace('\n', '')
         area_list = list(string)
-        lines = len(area_type.split('\n'))
-        rows = int(len(area_list) / lines)
-        coordinates = [(x, y) for x in range(lines) for y in range(rows)]
+        rows = len(area_type.split('\n'))
+        cols = int(len(area_list) / rows)
+        coordinates = [(x, y) for x in range(rows) for y in range(cols)]
+        print(area_list)
 
-        for i in area_list:
-            if i == 'O':
-                area_list[i] = Ocean()
-            elif i == 'M':
-                area_list[i] = Mountain()
-            elif i == 'D':
-                area_list[i] = Desert()
-            elif i == 'S':
-                area_list[i] = Savannah()
+        for ind, val in enumerate(area_list):
+            if val == 'O':
+                area_list[ind] = Ocean()
+            elif val == 'M':
+                area_list[ind] = Mountain()
+            elif val == 'D':
+                area_list[ind] = Desert()
+            elif val == 'S':
+                area_list[ind] = Savannah()
             else:
-                area_list[i] = Jungle()
+                area_list[ind] = Jungle()
 
-        for i in area_list:
-            self.map_dict[coordinates[i]] = area_list[i]
+        for ind, val in enumerate(area_list):
+            self.map_dict[coordinates[ind]] = area_list[ind]
         return self.map_dict
+
+    def check_string(self):
+        """
+        Check if the string input is one of the allowed categories
+        Check if the edges at the map is ocean
+        Check if the map is a square
+        """
+        accepted_landscape = ["J", "S", "D", "M", "O"]
+        for row in self.area_type:
+            for cell in row:
+                if cell not in accepted_landscape:
+                    raise ValueError(' That is an invalid landscape')
+        pass
 
     def populate_map(self, population):
         """
@@ -52,7 +59,6 @@ class Map:
         :return:
         """
         pass
-
 
     def find_cell(self, cell_to_find):
         """
@@ -85,20 +91,18 @@ class Map:
 
 
 if __name__ == "__main__":
-    map_string = """
-                  OOOOOOOOOOOOOOOOOOOOO
-                  OOOOOOOOSMMMMJJJJJJJO 
-                  OSSSSSJJJJMMJJJJJJJOO
-                  OSSSSSSSSSMMJJJJJJOOO
-                  OSSSSSJJJJJJJJJJJJOOO
-                  OSSSSSJJJDDJJJSJJJOOO
-                  OSSJJJJJDDDJJJSSSSOOO
-                  OOSSSSJJJDDJJJSOOOOOO
-                  OSSSJJJJJDDJJJJJJJOOO
-                  OSSSSJJJJDDJJJJOOOOOO
-                  OOSSSSJJJJJJJJOOOOOOO
-                  OOOSSSSJJJJJJJOOOOOOO
-                  OOOOOOOOOOOOOOOOOOOOO
-                  """
+    area_type = '''OOOOOOOOOOOOOOOOOOOOO
+OOOOOOOOSMMMMJJJJJJJO
+OSSSSSJJJJMMJJJJJJJOO
+OSSSSSSSSSMMJJJJJJOOO
+OSSSSSJJJJJJJJJJJJOOO
+OSSSSSJJJDDJJJSJJJOOO
+OSSJJJJJDDDJJJSSSSOOO
+OOSSSSJJJDDJJJSOOOOOO
+OSSSJJJJJDDJJJJJJJOOO
+OSSSSJJJJDDJJJJOOOOOO
+OOSSSSJJJJJJJJOOOOOOO
+OOOSSSSJJJJJJJOOOOOOO
+OOOOOOOOOOOOOOOOOOOOO'''
 
-    test_map = Map(map_string)
+    print(Map().create_map(area_type))
