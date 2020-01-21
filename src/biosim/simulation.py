fab26a58092a96590d5cff2a2904fccf62a6d559
Sjur klemetsen <sjkl@nmbu.no>
2020-01-21 23:33:08 +0100

    made the if name function

--- src/biosim/simulation.py
+++ src/biosim/simulation.py
@@ -362,35 +362,43 @@ class BioSim:
                 raise RuntimeError('ERROR: ffmpeg failed with: {}'.format(err))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    plt.ion()
+    geogr = """\
+               OOOOOOOOOOOOOOOOOOOOO
+               OOOOOOOOSMMMMJJJJJJJO
+               OSSSSSJJJJMMJJJJJJJOO
+               OSSSSSSSSSMMJJJJJJOOO
+               OSSSSSJJJJJJJJJJJJOOO
+               OSSSSSJJJDDJJJSJJJOOO
+               OSSJJJJJDDDJJJSSSSOOO
+               OOSSSSJJJDDJJJSOOOOOO
+               OSSSJJJJJDDJJJJJJJOOO
+               OSSSSJJJJDDJJJJOOOOOO
+               OOSSSSJJJJJJJJOOOOOOO
+               OOOSSSSJJJJJJJOOOOOOO
+               OOOOOOOOOOOOOOOOOOOOO"""
+    geogr = textwrap.dedent(geogr)
 
-    Geo = """\
-             OOOOOOO
-             OJSSDDO
-             OJSDDOO
-             OOOOOOO"""
-
-    ini_herbs = [{'loc': (1, 1),
+    ini_herbs = [{'loc': (10, 10),
                   'pop': [{'species': 'Herbivore',
                            'age': 5,
-                           'weight': 10}
-                          for _ in range(200)]}]
-    ini_carns = [{'loc': (2, 2),
+                           'weight': 20}
+                          for _ in range(150)]}]
+    ini_carns = [{'loc': (10, 10),
                   'pop': [{'species': 'Carnivore',
                            'age': 5,
-                           'weight': 60}
-                          for _ in range(20)]}]
-    sim = BioSim(Geo, ini_herbs, seed=123456)
-    sim.add_population(ini_carns)
-    sim.simulate(20)
-
-    #print(sim.num_animals_per_species)
-    #sim.plot_island_population()
-    #sim.heat_map_herbivore()
-    #sim.heat_map_carnivore()
-    #sim.heat_map_carnivore()
-    #sim.setup_graphics()
-    #sim.standard_map()
-
-
-
+                           'weight': 20}
+                          for _ in range(40)]}]
+    sim = BioSim(island_map=geogr, ini_pop=ini_herbs,
+                 seed=123456)
+    sim.set_animal_parameters('Herbivore', {'zeta': 3.2, 'xi': 1.8})
+    sim.set_animal_parameters('Carnivore', {'a_half': 70, 'phi_age': 0.5,
+                                            'omega': 0.3, 'F': 65,
+                                            'DeltaPhiMax': 9.})
+    sim.set_landscape_parameters('J', {'f_max': 700})
+    sim.simulate(num_years=10, vis_years=1, img_years=2000)
+    sim.add_population(population=ini_carns)
+    sim.simulate(num_years=10, vis_years=1, img_years=2000)
+    plt.savefig('check_sim.pdf')
+    input('Press ENTER')
